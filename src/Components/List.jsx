1c15704e5e718457409dbf073095df5adfa05dfe
Marlon C.S <marlsas0306@gmail.com>
2021-12-22 23:26:22 -0500

    Update list functionality

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,10 +1,19 @@
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../Context/AppProvider";
+
 function List(props) {
+    const { searchText } = useContext(AppContext);
     const list = props.pokemons;
+    const [pokemonListFiltered, setPokemonList] = useState([]);
+
+    useEffect(() => {
+        setPokemonList(list.filter(p => p.name.includes(searchText)));
+    }, [searchText]);
 
     return (
         <ul>
             {
-                list.map((pokemon, index) =>
+                pokemonListFiltered.map((pokemon, index) =>
                     <li key={index}>{pokemon.name}</li>
                 )
             }
