36407fb54f9b889ee89ad1228202981c1b83b7bc
Marlon C.S <marlsas0306@gmail.com>
2021-12-22 21:09:42 -0500

    Add list component to home

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,11 +1,17 @@
 import "./Home.css"
 import Search from "../Components/Search";
+import List from "../Components/List";
+import { useContext } from "react";
+import { AppContext } from "../Context/AppProvider";
 
 function Home() {
+    const { pokemons } = useContext(AppContext);
+    
     return (
         <div className="Home-wrapper">
             <Search />
             <div className="poke-list-wrapper">
+                <List pokemons={pokemons} ></List>
             </div>
         </div>
     );
