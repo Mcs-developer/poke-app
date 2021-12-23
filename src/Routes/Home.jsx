32349346c0848ce1116300a81783adb706592f25
Marlon C.S <marlsas0306@gmail.com>
2021-12-23 00:09:05 -0500

    Update list functionality

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,17 +1,14 @@
 import "./Home.css"
 import Search from "../Components/Search";
 import List from "../Components/List";
-import { useContext } from "react";
-import { AppContext } from "../Context/AppProvider";
 
 function Home() {
-    const { pokemons } = useContext(AppContext);
     
     return (
         <div className="Home-wrapper">
             <Search />
             <div className="poke-list-wrapper">
-                <List pokemons={pokemons} ></List>
+                <List></List>
             </div>
         </div>
     );
